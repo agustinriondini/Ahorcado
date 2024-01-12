1d922b02dbec8f9fceabeb85932cf7e9807de8e0
Agustin Riondini <105334088+agustinriondini@users.noreply.github.com>
2024-01-12 20:12:24 +0000

    -Ver changelog.txt

--- docs/scripts/selector.js
+++ docs/scripts/selector.js
@@ -3,6 +3,8 @@ function pruebaPiloto() {
     var selectElement = document.getElementById("selectDificultad");
     var selectedValue = selectElement.value;
 
+    localStorage.setItem("dificultadSeleccionada", selectedValue);
+
     if (selectedValue === "dificil") {
 
         var dificultadIncrementada = document.createElement("script");
@@ -24,8 +26,15 @@ function pruebaPiloto() {
 
         document.head.appendChild(dificultadIncrementada);
     }else{
-        location.reload();
+        //-->location.reload();<--- no usar, crea un loop infinito
+        return; //<-- usar
     }
-    
-
 }
+window.onload = function () {
+    var storedValue = localStorage.getItem("dificultadSeleccionada");
+    if (storedValue) {
+        var selectElement = document.getElementById("selectDificultad");
+        selectElement.value = storedValue;
+    }
+    pruebaPiloto();
+};
